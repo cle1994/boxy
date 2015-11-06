01aaba26d53f24b84ac7f75e816df333b16be1ac
Christian Le <christianle94@gmail.com>
2015-11-05 17:07:52 -0800

    Initial Commit

---
+++ Boxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Boxy
+//
+//  Created by Christian Le on 11/5/15.
+//  Copyright © 2015 christianle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
